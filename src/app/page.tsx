c9e81e935df705e1bfbd237598b7f2e49eea4082
Renan José de Oliveira <oliveiira2012@gmail.com>
2025-07-10 12:02:46 +0000

    este é o link do SIC: https://intranet.brsupply.com.br/Intranet/index.ph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import Link from "next/link";
 import { LoginPageClient } from "@/components/auth/login-page-client";
-import { Globe, Briefcase, Linkedin, Youtube, Info, Shield, Facebook, Waves } from "lucide-react";
+import { Globe, Briefcase, Linkedin, Youtube, Shield, Facebook, Waves } from "lucide-react";
 
 
 export default async function LoginPage() {
   
+  // Custom SVG icon for SIC to match the other icons' style
+  const SicIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <rect width="18" height="18" x="3" y="3" rx="2" />
+      <path d="M9 15V9h2.5a2.5 2.5 0 0 1 0 5H9" />
+      <path d="M14.5 15V9" />
+      <path d="M14.5 12a2.5 2.5 0 0 1 3-2.5" />
+    </svg>
+  );
+
   const footerLinks = [
     { name: "Site", href: "https://brsupply.com.br/", icon: Globe },
     { name: "Supply Manager", href: "#", icon: Briefcase },
@@ -12,7 +33,7 @@ export default async function LoginPage() {
     { name: "LinkedIn", href: "https://www.linkedin.com/company/brsupply-suprimentos-corporativos/posts/?feedView=all", icon: Linkedin },
     { name: "Youtube", href: "https://www.youtube.com/@brsupplysuprimentos", icon: Youtube },
     { name: "Facebook", href: "https://www.facebook.com/brsupply.suprimentos.corporativos", icon: Facebook },
-    { name: "SIC", href: "#", icon: Info },
+    { name: "SIC", href: "https://intranet.brsupply.com.br/Intranet/index.php", icon: SicIcon },
     { name: "Contato Seguro", href: "#", icon: Shield },
   ];
 
