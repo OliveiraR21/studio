bc1b6054b634ba00c4882465e7104271b02d9faa
Your Name <oliveiira2012@gmail.com>
2025-07-10 02:19:51 +0000

    ctrl+k novamente sem aparecer a barra para pesquisa

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -40,10 +40,6 @@ const DialogContent = React.forwardRef<
       ref={ref}
       className={cn(
         "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-xl",
-        "p-0", // reset padding for animated border
-        "relative before:absolute before:inset-0 before:z-[-1] before:rounded-xl before:p-[1px]",
-        "before:bg-gradient-to-r before:from-primary before:to-blue-500",
-        "data-[state=open]:before:bg-[length:200%_200%] data-[state=open]:before:animate-border-spin",
         className
       )}
       {...props}
