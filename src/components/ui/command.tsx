b3f4b36a18cc6765659c44e3d6154267af26dd5d
Your Name <oliveiira2012@gmail.com>
2025-07-10 02:22:36 +0000

    vamos tentar colocar a borda novamente? quero muito ela, vai ser algo mu

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -29,11 +29,13 @@ interface CommandDialogProps extends DialogProps {}
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg">
-        <DialogTitle className="sr-only">Busca Global</DialogTitle>
-        <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
-          {children}
-        </Command>
+      <DialogContent className="overflow-hidden p-0 shadow-lg bg-transparent border-0">
+        <div className="relative rounded-xl bg-gradient-to-r from-primary to-blue-500 p-0.5 data-[state=open]:animate-border-spin">
+            <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+            <DialogTitle className="sr-only">Busca Global</DialogTitle>
+            {children}
+            </Command>
+        </div>
       </DialogContent>
     </Dialog>
   )
