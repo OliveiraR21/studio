ce5ba63cd3b62f64108c467a6ff1da67b262efd6
Your Name <oliveiira2012@gmail.com>
2025-07-10 02:28:03 +0000

    gostei da ideia de algo dinâmico, mas ainda não está legal, veja

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,15 +89,10 @@ export default {
             height: '0',
           },
         },
-        'border-spin': {
-            '0%': { 'transform': 'rotate(0deg)' },
-            '100%': { 'transform': 'rotate(360deg)' },
-        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'border-spin': 'border-spin 3s linear infinite',
       },
     },
   },
